b3ebbf3f9e2d1246209823d1590deaaac0b46b5f
Satyam Jaiswal <96405804+Satyam-jsw@users.noreply.github.com>
2023-08-26 19:44:14 +0530

    initial commit

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,24 +1,27 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const url = 'https://ai-chatgptrix.onrender.com'
+
+// const url="http://127.0.0.1:8080";
 
 module.exports = function (app) {
   app.use(
     'api/v1/auth/register',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8080',
+      target: url,
       changeOrigin: true,
     })
   ),
   app.use(
     'api/v1/auth/login',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8080',
+      target: url,
       changeOrigin: true,
     })
   ),
   app.use(
     'api/v1/auth/logout',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8080',
+      target: url,
       changeOrigin: true,
     })
   );
