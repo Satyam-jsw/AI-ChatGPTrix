d6dd59f3b328e3c517c363eb5381fd246c5dec50
Satyam Jaiswal <96405804+Satyam-jsw@users.noreply.github.com>
2023-08-27 20:41:53 +0530

    Update setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,21 +5,21 @@ const url = 'https://ai-chatgptrix.onrender.com'
 
 module.exports = function (app) {
   app.use(
-    'api/v1/auth/register',
+    '/api/v1/auth/register',
     createProxyMiddleware({
       target: url,
       changeOrigin: true,
     })
   ),
   app.use(
-    'api/v1/auth/login',
+    '/api/v1/auth/login',
     createProxyMiddleware({
       target: url,
       changeOrigin: true,
     })
   ),
   app.use(
-    'api/v1/auth/logout',
+    '/api/v1/auth/logout',
     createProxyMiddleware({
       target: url,
       changeOrigin: true,
