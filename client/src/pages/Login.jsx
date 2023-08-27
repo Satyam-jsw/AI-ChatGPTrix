4608e4d0a7f58d402d8e36b9a9c95fae19054add
Satyam Jaiswal <96405804+Satyam-jsw@users.noreply.github.com>
2023-08-27 20:25:35 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://127.0.0.1:8080/api/v1/auth/login", { email, password });
+      await axios.post("/api/v1/auth/login", { email, password });
       toast.success("Logged in Successfully");
       localStorage.setItem("authToken", true);
       navigate("/");
