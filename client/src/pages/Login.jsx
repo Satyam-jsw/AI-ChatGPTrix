33c40805b247bb37fcce396666174d810a8bd148
Satyam Jaiswal <96405804+Satyam-jsw@users.noreply.github.com>
2023-12-26 23:39:07 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("/api/v1/auth/login", { email, password });
+      await axios.post("https://ai-chat-gp-trix-az8n.vercel.app/api/v1/auth/login", { email, password });
       toast.success("Logged in Successfully");
       localStorage.setItem("authToken", true);
       navigate("/");
